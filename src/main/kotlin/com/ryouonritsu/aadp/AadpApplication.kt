017562947ff6caa379fcdffb23b52c17863e251d
zhouhongxi <zhouhongxi@bytedance.com>
2022-11-01 14:46:42 +0800

    Initial commit

---
+++ src/main/kotlin/com/ryouonritsu/aadp/AadpApplication.kt
@@ -0,0 +1,11 @@
+package com.ryouonritsu.aadp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AadpApplication
+
+fun main(args: Array<String>) {
+    runApplication<AadpApplication>(*args)
+}
---
+++ src/test/kotlin/com/ryouonritsu/aadp/AadpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ryouonritsu.aadp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AadpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
