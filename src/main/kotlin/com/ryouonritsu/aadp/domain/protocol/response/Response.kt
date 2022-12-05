a0dd778f6fb97f1d6798e6beed2c27f43512af8f
zhouhongxi <zhouhongxi@bytedance.com>
2022-12-05 12:03:48 +0800

    新增返回基类

---
+++ src/main/kotlin/com/ryouonritsu/aadp/domain/protocol/response/Response.kt
@@ -0,0 +1,30 @@
+package com.ryouonritsu.aadp.domain.protocol.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+/**
+ * @author ryouonritsu
+ */
+@Schema(description = "Response")
+class Response<T>(
+    @Schema(name = "success", description = "Success状态", required = true)
+    val success: Boolean,
+    @Schema(name = "message", description = "信息", required = true)
+    val message: String,
+    @Schema(name = "data", description = "数据")
+    val data: T?
+) {
+    companion object {
+        fun <T> success(data: T): Response<T> {
+            return Response(true, "success", data)
+        }
+
+        fun <T> success(message: String, data: T): Response<T> {
+            return Response(true, message, data)
+        }
+
+        fun <T> error(message: String): Response<T> {
+            return Response(false, message, null)
+        }
+    }
+}
