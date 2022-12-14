52c5b959dd432e400a1808b0b94d99745b97ae30
kude77 <412724618@qq.com>
2022-12-14 13:40:05 +0800

    论文专题相关接口

--- src/main/kotlin/com/ryouonritsu/aadp/controller/PaperController.kt
+++ src/main/kotlin/com/ryouonritsu/aadp/controller/PaperController.kt
@@ -2,7 +2,6 @@ package com.ryouonritsu.aadp.controller
 
 import com.ryouonritsu.aadp.common.annotation.AuthCheck
 import com.ryouonritsu.aadp.service.PaperService
-import com.ryouonritsu.aadp.utils.RedisUtils
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -16,8 +15,7 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/paper")
 @Tag(name = "论文接口")
 class PaperController(
-    private val paperService: PaperService,
-    private val redisUtils: RedisUtils
+    private val paperService: PaperService
 ) {
     @GetMapping("/searchPaperByKeyword")
     @AuthCheck
@@ -41,6 +39,48 @@ class PaperController(
             description = "每页数据条数，默认为10"
         ) limit: Int
     ) = paperService.searchPaperByKeyword(keyword, page, limit)
+
+    @GetMapping("/searchPaperByKeywordAndSubject")
+    @AuthCheck
+    @Tag(name = "论文接口")
+    @Operation(summary = "根据给出的关键词在标题中模糊查询论文并筛选指定专题")
+    fun searchPaperByKeywordAndSubject(
+        @RequestHeader(
+            name = "token",
+            required = true
+        ) token: String,
+        @RequestParam("keyword") @Parameter(
+            description = "查询关键词",
+            required = true
+        ) keyword: String,
+        @RequestParam("subject") @Parameter(
+            description = "专题学科关键词",
+            required = true
+        ) subject: String,
+        @RequestParam("page", required = false, defaultValue = "1")
+        @Parameter(
+            description = "查询页数，从1开始，默认为1"
+        ) page: Int,
+        @RequestParam("limit", required = false, defaultValue = "10")
+        @Parameter(
+            description = "每页数据条数，默认为10"
+        ) limit: Int
+    ) = paperService.searchPaperByKeyword(keyword, subject, page, limit)
+
+    @GetMapping("/searchPaperResultInfoByKeyword")
+    @AuthCheck
+    @Tag(name = "论文接口")
+    @Operation(summary = "根据给出的关键词在标题中模糊查询论文的结果，返回结果总数和专题")
+    fun searchPaperResultInfoByKeyword(
+        @RequestHeader(
+            name = "token",
+            required = true
+        ) token: String,
+        @RequestParam("keyword") @Parameter(
+            description = "查询关键词",
+            required = true
+        ) keyword: String,
+    ) = paperService.searchPaperByKeyword(keyword)
 
     @GetMapping("/getTop10PaperByClick")
     @AuthCheck
--- src/main/kotlin/com/ryouonritsu/aadp/service/PaperService.kt
+++ src/main/kotlin/com/ryouonritsu/aadp/service/PaperService.kt
@@ -1,6 +1,7 @@
 package com.ryouonritsu.aadp.service
 
 import com.ryouonritsu.aadp.domain.dto.PaperDTO
+import com.ryouonritsu.aadp.domain.dto.PaperResultInfoDTO
 import com.ryouonritsu.aadp.domain.protocol.response.Response
 
 /**
@@ -10,7 +11,9 @@ import com.ryouonritsu.aadp.domain.protocol.response.Response
 interface PaperService {
     fun searchPaperByKeyword(keyword: String, page: Int, limit: Int): Response<List<PaperDTO>>
 
-    fun searchPaperByKeyword(keyword: String): Response<List<PaperDTO>>
+    fun searchPaperByKeyword(keyword: String, subject: String, page: Int, limit: Int): Response<List<PaperDTO>>
+
+    fun searchPaperByKeyword(keyword: String): Response<PaperResultInfoDTO>
 
     fun getTop10PaperByClick(): Response<List<PaperDTO>>
 }
